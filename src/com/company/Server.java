6167aace326ec7086552ee817347e65ea07b4076
Tselishchevns <tselishchevns@gmail.com>
2021-08-02 21:05:00 +0300

    Добавлены методы в класс сервер.

--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -77,6 +77,39 @@ private static class Handler extends Thread{
          connection.send(new Message(MessageType.USER_ADDED, name));
       }
    }
+   private void serverMainLoop (Connection connection, String userName) throws IOException, ClassNotFoundException{
+      while (true){
+         Message message = connection.receive();
+         if (message.getType() == MessageType.TEXT){
+            String data = message.getData();
+            sendBroadcastMessage(new Message(MessageType.TEXT, userName + ": " + data));
+         }else {
+            ConsoleHelper.writeMessage("Получено сообщение от " + socket.getRemoteSocketAddress() + ". Тип сообщения не соответствует формату.");
+         }
+      }
+   }
+
+   @Override
+   public void run(){
+      ConsoleHelper.writeMessage("Установленно новое соединение с " + socket.getRemoteSocketAddress());
+      String userName = null;
+      try(Connection connection = new Connection(socket)) {
+         userName = serverHandshake(connection);
+         sendBroadcastMessage(new Message(MessageType.USER_ADDED, userName)); //сообщение всем об подключение нового пользователя
+         notifyUsers(connection, userName);
+         serverMainLoop(connection, userName);
+
+
+      } catch (IOException | ClassNotFoundException e){
+         ConsoleHelper.writeMessage("Ошибка при обмене данных");
+      }
+      if (userName != null){
+         connectionMap.remove(userName);
+         sendBroadcastMessage(new Message(MessageType.USER_REMOVED, userName));
+      }
+      ConsoleHelper.writeMessage("Соединение с " + socket.getRemoteSocketAddress() + " закрыто.");
+
+   }
 }
 }
 
