9143fc09817f2d18947bbdb113b416835f7c58a5
Tselishchevns <tselishchevns@gmail.com>
2021-08-02 22:16:02 +0300

    Добавлены методы в класс client.

--- src/com/company/client/Client.java
+++ src/com/company/client/Client.java
@@ -27,6 +27,21 @@ protected String getUserName(){
 }
 
 public class SocketThread extends Thread{
+   protected void processIncomingMessage(String message){
+      ConsoleHelper.writeMessage(message);
+   }
+   protected void informAboutAddingNewUser(String userName){
+      ConsoleHelper.writeMessage("К чату подклчился " + userName);
+   }
+   protected void informAboutDeletingNewUser(String userName){
+      ConsoleHelper.writeMessage(userName + " вышел из чата.");
+   }
+   protected void notifyConnectionStatusChanged(boolean clientConnected){
+      Client.this.clientConnected = clientConnected;
+      synchronized (Client.this){
+         Client.this.notify();
+      }
+   }
 
 }
 
